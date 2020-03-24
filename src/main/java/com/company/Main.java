58e7b4fe06c9863f4948b77da326448ecb3297ca
David <duprazdavid@gmail.com>
2020-03-24 08:21:44 +0100

    final server

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException
+    {
+        Server srv=new Server();
+        //Client wernerHerzog=new Client("127.0.0.1");
+
+        //wernerHerzog.
+    }
+}
